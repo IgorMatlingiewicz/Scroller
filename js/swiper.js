af5a71ba59bdb5895194ba3388fad281af319a49
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2022-12-28 18:27:31 +0100

    Fixed styles of img tag, add swiper to detect swipes on touchable devices

---
+++ js/swiper.js
@@ -0,0 +1,60 @@
+class Swiper {
+    constructor() {
+        this.inicialY = null;
+        this.inicialX = null;
+
+        document.addEventListener('touchstart', (event) => this.startTouch(event));
+        document.addEventListener('touchmove', (event) => this.moveTouch(event));
+
+        this.events = {
+            swipeUp: new Event('swipeUp'),
+            swipeDown: new Event('swipeDown'),
+            swipeLeft: new Event('swipeLeft'),
+            swipeRight: new Event('swipeRight'),
+        }
+    }
+
+    startTouch(event) {
+        event.preventDefault();
+
+        this.inicialX = event.touches[0].clientX;
+        this.inicialY = event.touches[0].clientY;
+    }
+
+    moveTouch(event) {
+        if (!this.inicialX || !this.inicialY) return;
+
+        const currentX = event.touches[0].clientX;
+        const currentY = event.touches[0].clientY;
+
+        const diffX = this.initialX - currentX;
+        const diffY = this.inicialY - currentY;
+
+        console.log(diffX);
+        console.log(diffY);
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            //jestesmy w osi X
+            if (diffX > 0){
+                //w lewo
+                document.dispatchEvent(this.events.swipeLeft);
+            } else {
+                //w prawo
+                document.dispatchEvent(this.events.swipeRight);
+            }   
+        } else {
+            //jesteśmy w osi Y
+            if (diffY > 0){
+                // w góre
+                document.dispatchEvent(this.events.swipeUp);
+            } else {
+                // w dół
+                document.dispatchEvent(this.events.swipeDown);
+            }
+        }
+
+        this.inicialX = null;
+        this.inicialY = null;
+    }
+}
+
+new Swiper();
